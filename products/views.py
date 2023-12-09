51c69af47b679a4963ec26374aff4c72db3c0548
SahiraAwad <saherahussein1988@gmail.com>
2023-12-09 16:57:49 +0300

    add CBV

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DeleteView
+
+from .models import Product, Brand , Review
+
 # Create your views here.
+
+
+class ProductList(ListView):
+    model = Product
+
+class ProductDetail(DeleteView):
+    model = Product
