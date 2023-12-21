1b211f1929b8ed509a0a9505644e652772833361
SahiraAwad <saherahussein1988@gmail.com>
2023-12-21 21:46:24 +0300

    show product reviews and images from views

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
-from .models import Product, Brand , Review
+from .models import Product, Brand , Review , ProductImages
 
 # Create your views here.
 
@@ -12,3 +12,14 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product
+
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["reviwes"] = Review.objects.filter(product=self.get_object())
+        context["images"] = ProductImages.objects.filter(product=self.get_object())
+        return context
+    
+
+    
